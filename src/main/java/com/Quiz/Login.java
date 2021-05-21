5f6b0da4414b8fb10d7566aad3bdbf9554f0f895
SAYEED AJMAL <SAYEEDAJMALA06@GMAIL.COM>
2021-05-21 14:33:40 +0000

    added Database connection to for Authentication

--- src/main/java/com/Quiz/Login.java
+++ src/main/java/com/Quiz/Login.java
@@ -3,13 +3,13 @@ package com.Quiz;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -35,19 +35,39 @@ public class Login implements Initializable {
 
     /* Accessing login */
     public void login(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Dashboard.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        scene.getStylesheets().add("/styles/style.css");
-        stage.setTitle("Login");
-        stage.setFullScreen(true);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setFullScreenExitHint("");
-        stage.setMaximized(true);
-        stage.show();
-        System.out.println("Clicked Login");
-        ((Stage) ((Button) event.getSource()).getScene().getWindow()).close();
+        String Query_for_login = "select * from " + login_name.getText().toUpperCase()
+                + " where username=? and password=?";
+        try {
+            String username = login_name.getText();
+            String password = login_password.getText();
+            PreparedStatement preparedStatement = connection.prepareStatement(Query_for_login);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            if (!username.isBlank() && !password.isBlank()) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/Dashboard.fxml"));
+                    Scene scene = new Scene(root);
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    scene.getStylesheets().add("/styles/style.css");
+                    stage.setTitle("Login");
+                    stage.setFullScreen(true);
+                    stage.initModality(Modality.APPLICATION_MODAL);
+                    stage.setFullScreenExitHint("");
+                    stage.setMaximized(true);
+                    stage.show();
+                    System.out.println("Login Sucessfully");
+                    ((Stage) ((Button) event.getSource()).getScene().getWindow()).close();
+                } else {
+                    System.out.println("Username or Password is Invalid");
+                }
+            } else {
+                System.out.println("Fill out All Fields");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /* link for returning into Signup Page */
